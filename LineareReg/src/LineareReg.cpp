2d00f5b394266f1dc6040a4f032bf61c739ea720
Thomas Rometsch <thomas.rometsch@gmail.com>
2017-03-28 14:10:15 +0200

    finish calculation for fitting of linear function

--- LineareReg/src/LineareReg.cpp
+++ LineareReg/src/LineareReg.cpp
@@ -8,26 +8,83 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
+
+vector<double> fit_linear_function(vector<double> x, vector<double> y, vector<double> y_err);
+vector<double> fit_proportionality(vector<double> x, vector<double> y, vector<double> y_err);
+
 
 int main() {
 	// Spannungs als X-Werte
-	double x[] = {0.5, 1,	1.5, 2,	2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6};
+	vector<double> x = {0.5, 1,	1.5, 2,	2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6};
 	// Strom als Y-Werte
-	double y[] = {0.065, 0.206, 0.405, 0.492, 0.606, 0.782, 0.865, 1.018, 1.199, 1.327, 1.408, 1.627};
-	double err[] = {0.01, 0.01, 0.02, 0.02, 0.02, 0.02, 0.02, 0.03, 0.03, 0.03, 0.03, 0.04};
+	vector<double> y = {0.065, 0.206, 0.405, 0.492, 0.606, 0.782, 0.865, 1.018, 1.199, 1.327, 1.408, 1.627};
+	vector<double> err = {0.01, 0.01, 0.02, 0.02, 0.02, 0.02, 0.02, 0.03, 0.03, 0.03, 0.03, 0.04};
 
-	unsigned int L = 12; // Anzahl der Messwerte
+	{
+		vector<double> fit_parameters = fit_linear_function(x,y,err);
 
+		double a = fit_parameters[0];
+		double var_a = fit_parameters[2];
+		double b = fit_parameters[1];
+		double var_b = fit_parameters[3];
+		double chisq = fit_parameters[4];
 
-	return 0;
+		double R = 1/b;
+		double stddev_R = sqrt(var_b)/b/b;
+		double I0 = a;
+		double stddev_I0 = sqrt(var_a);
+
+		cout 	<< "====================================" << endl
+				<< "====================================" << endl
+				<< "Fit: y = a + bx" << endl
+				<< scientific
+				<< "a = " << a << endl
+				<< "Var(a) = " << var_a << endl
+				<< "b = " << b << endl
+				<< "Var(b) = " << var_b << endl
+				<< "chisq/(N-2) = " << chisq << endl
+				<< "====================================" << endl
+				<< "R = " << R << endl
+				<< "stddev_R = " << stddev_R << endl
+				<< "I0 = " << I0 << endl
+				<< "stddev_I0 = " << stddev_I0 << endl
+				<< "====================================" << endl
+				<< "====================================" << endl;
+	}
+	cout << endl;
+
+	{
+		vector<double> fit_parameters = fit_proportionality(x,y,err);
+
+		double b = fit_parameters[0];
+		double var_b = fit_parameters[1];
+		double chisq = fit_parameters[2];
+
+		double R = 1/b;
+		double stddev_R = sqrt(var_b)/b/b;
+
+		cout 	<< "====================================" << endl
+				<< "====================================" << endl
+				<< "Fit: y = bx" << endl
+				<< scientific
+				<< "b = " << b << endl
+				<< "Var(b) = " << var_b << endl
+				<< "chisq/(N-2) = " << chisq << endl
+				<< "====================================" << endl
+				<< "R = " << R << endl
+				<< "stddev_R = " << stddev_R << endl
+				<< "====================================" << endl
+				<< "====================================" << endl;
+	}
 }
 
-vector<double> lin_reg_1st_pol(vector<double> x, vector<double> y, vector<double> y_err) {
+vector<double> fit_linear_function(vector<double> x, vector<double> y, vector<double> y_err) {
 	/* Lineare Regression der Funktion y = f(x) = a*x + b
-	 * Zurueckgegeben wird ein Vector mit den Eintraegen (a,b,var_a,var_b)
+	 * Zurueckgegeben wird ein Vector mit den Eintraegen (a,b,var_a,var_b,chisq)
 	 */
-	unsigned int L = x.size();
+	unsigned int N = x.size();
 
 	// Hilfsgrößen berechnen
 	double S = 0;
@@ -36,8 +93,8 @@ vector<double> lin_reg_1st_pol(vector<double> x, vector<double> y, vector<double
 	double Sxx = 0;
 	double Sxy = 0;
 
-	for (unsigned int i=0; i<L; i++) {
-		double invvar = y_err[i]*y_err[i];
+	for (unsigned int i=0; i<N; i++) {
+		double invvar = 1.0/(y_err[i]*y_err[i]);
 		S += invvar;
 		Sx += x[i]*invvar;
 		Sy += y[i]*invvar;
@@ -51,5 +108,59 @@ vector<double> lin_reg_1st_pol(vector<double> x, vector<double> y, vector<double
 	double var_a = Sxx/Det;
 	double var_b = S/Det;
 
-	// TODO: Return
+	double chisq = 0;
+	for (unsigned int i=0; i<N; i++) {
+		double X = (y[i] - a - b*x[i])/y_err[i];
+		chisq += X*X;
+	}
+	chisq = chisq/(N-2);
+
+	vector<double> res;
+	res.push_back(a);
+	res.push_back(b);
+	res.push_back(var_a);
+	res.push_back(var_b);
+	res.push_back(chisq);
+
+	return res;
+}
+
+vector<double> fit_proportionality(vector<double> x, vector<double> y, vector<double> y_err) {
+	/* Lineare Regression der Funktion y = f(x) = a*x
+	 * Zurueckgegeben wird ein Vector mit den Eintraegen (a,var_a,chisq)
+	 */
+	unsigned int N = x.size();
+
+	// Hilfsgrößen berechnen
+	double S = 0;
+	double Sx = 0;
+	double Sy = 0;
+	double Sxx = 0;
+	double Sxy = 0;
+
+	for (unsigned int i=0; i<N; i++) {
+		double invvar = 1.0/(y_err[i]*y_err[i]);
+		S += invvar;
+		Sx += x[i]*invvar;
+		Sy += y[i]*invvar;
+		Sxx += x[i]*x[i]*invvar;
+		Sxy += x[i]*y[i]*invvar;
+	}
+
+	double a = Sxy/Sxx;
+	double var_a = 1.0/Sxx;
+
+	double chisq = 0;
+	for (unsigned int i=0; i<N; i++) {
+		double X = (y[i] - a*x[i])/y_err[i];
+		chisq += X*X;
+	}
+	chisq = chisq/(N-2);
+
+	vector<double> res;
+	res.push_back(a);
+	res.push_back(var_a);
+	res.push_back(chisq);
+
+	return res;
 }
